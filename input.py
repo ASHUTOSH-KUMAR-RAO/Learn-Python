2ab4c43185926ff7a3ef49fcabee136a3e867a6b
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-09-01 14:51:37 +0530

    02:internship starting

---
+++ input.py
@@ -0,0 +1,8 @@
+
+age = int(input("Enter Your age :- 👉 \t"))
+if age > 18:
+    print("you Can Apply for Licence ! ✅")
+
+elif age <= 18:
+        print("Sorry For inconvenience Your Age Limit Below 18 ! ❌")
+
